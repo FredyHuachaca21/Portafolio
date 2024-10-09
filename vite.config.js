6513e1f37878cb3cbf1a8b7d0b22f14307aadc2d
Fredy Huachaca <fredyhuachaca@MacBook-Pro-de-Fredy.local>
2024-10-08 19:05:07 -0500

    Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Portafolio/',
+  build: {
+    outDir: 'docs'  // Esto es importante
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   }
